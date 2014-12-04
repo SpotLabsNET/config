cf3f8cebbea896e7d6c598cb10e632e909a59420
Jevon Wright <jevon@jevon.org>
2014-12-05 00:55:35 +1300

    Empty initial framework

---
+++ src/Config.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Openclerk;
+
+class Config {
+  // TODO whatever
+  // TODO maybe initialise from .json?
+  static function get($key, $default = null) {
+    return "42";
+  }
+}
