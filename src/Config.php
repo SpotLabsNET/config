c03958b1192e8eada95eabcdd1ea3569d72521a9
Jevon Wright <jevon@jevon.org>
2014-12-05 01:01:29 +1300

    Implement Config::merge() and Config::overwrite()

--- src/Config.php
+++ src/Config.php
@@ -5,6 +5,10 @@ namespace Openclerk;
 class Config {
   static $values = array();
 
+  /**
+   * Get the runtime site configuration variable named {@code $key}.
+   * @throws ConfigException if config has not been initialised or there is no valid key (and no default has been provided)
+   */
   static function get($key, $default = null) {
     if (isset(Config::$values[$key])) {
       return Config::$values[$key];
@@ -19,6 +23,21 @@ class Config {
     }
   }
 
-  // TODO merge
-  // TODO load from JSON
+  /**
+   * If the input arrays have the same string keys, then the earlier value for that key will not be overwritten.
+   * @see #overwrite()
+   */
+  static function merge($values) {
+    Config::$values += $values;
+  }
+
+  /**
+   * If the input arrays have the same string keys, then the later value for that key will overwrite the previous one.
+   * @see #merge()
+   */
+  static function overwrite($values) {
+    Config::$values = array_merge(Config::$values, $values);
+  }
+
+  // TODO load from JSON?
 }
