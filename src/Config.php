ec9283bc80a96457585891d8ede6626e18a9ecfc
Jevon Wright <jevon@jevon.org>
2015-03-18 13:27:49 +1300

    Add Config::isEmpty() helper method
    So that we can detect whether Config::get() will crash since it's not
    initialised or not

--- src/Config.php
+++ src/Config.php
@@ -13,7 +13,7 @@ class Config {
     if (isset(Config::$values[$key])) {
       return Config::$values[$key];
     } else {
-      if (empty(Config::$values)) {
+      if (Config::isEmpty()) {
         throw new ConfigException("Site configuration has not been initialised for '$key'");
       } else if ($default !== null) {
         return $default;
@@ -41,6 +41,14 @@ class Config {
    */
   static function overwrite($values) {
     Config::$values = array_merge(Config::$values, $values);
+  }
+
+  /**
+   * Has configuration not been initialised yet - i.e. there have been no calls to
+   * {@link Config#merge()} or {@link Config#overwrite()}?
+   */
+  static function isEmpty() {
+    return empty(Config::$values);
   }
 
   // TODO load from JSON?
